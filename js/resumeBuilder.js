35aa15ca8fbbe886b5b75403eef250b45794f91a
seanmyers <sean.myers@nbtsolutions.com>
2015-02-01 11:50:02 -0500

    changes made just before I deleted all the code in resumbuilder.js with the exception of the JSON objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,14 +3,26 @@ var bio  = {
   "role" : "Co-Founder NBT Solutions LLC",
   "contactInfo" : {
     "email" : "sean.myers@nbtsolutions.com",
-    "twitter" : "@seandmyersd",
+    "twitter" : "@seandmyers",
     "github" : "http://github.com/seanmyers",
     "phone" : "+1 716 359-3586",
   },
   "picture" : "images/sean.png",
   "welcomeMessage" : "Experienced Geospatial Engineer and Developer",
   "skills" : ["Product Management", "Geographic Information Systems", "Front End Programming"]
-};
+}
+
+var work = {};
+  work.employer = "NBT Solutions";
+  work.startdate = "January 2009";
+  work.title = "Product C-Founder";
+  work.years = 5;
+  work.locaation = "Buffalo, NY";
+
+var education = {};
+  education["name"] = "University of New Hampshire";
+  education["years"] = "1988 - 1992";
+  education["city"] = "Durham, NH";
 
 var formattedName = HTMLheaderName.replace("%data%",bio.name);
 var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
@@ -19,7 +31,13 @@ var formattedPicture = HTMLbioPic.replace("%data%",bio.picture);
 var formattedMessage = HTMLWelcomeMsg.replace("%data%",bio.welcomeMessage);
 var formattedTwitter = HTMLtwitter.replace("%data%",bio.contactInfo.twitter);
 var formattedGithub = HTMLgithub.replace("%data%",bio.contactInfo.github);
-console.log(formattedTwitter);
+
+/* Format employment and education information */
+
+var formattedStart = HTMLworkStart.replace("%data%",work.startdate);
+var formattedEmployer = HTMLworkEmployer.replace("%data%",work.employer);
+
+var formattedSchool = HTML
 
 $("#header").prepend(formattedMessage);
 $("#header").prepend(formattedRole);
@@ -33,6 +51,10 @@ $("#header").append(HTMLskillsStart);
 */
 for (i = 0; i < bio.skills.length; i++) {
   var formattedSkills = HTMLskills.replace("%data%",bio.skills[i]);
-  console.log(formattedSkills);
   $("#skills").append(formattedSkills);
 };
+
+/* Add formattted employment and educaiton information */
+
+$("#workExperience").prepend(formattedStart);
+$("#workExperience").append(formattedEmployer);

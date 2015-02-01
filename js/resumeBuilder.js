060ec10b5b692f0cad8b461db3aa34fdc05e267c
seanmyers <sean.myers@nbtsolutions.com>
2015-02-01 13:22:52 -0500

    simple changes to resume JSON values...blacked out places where previously nothing would display

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,60 +1,77 @@
 var bio  = {
-  "name" : "Sean Myers",
-  "role" : "Co-Founder NBT Solutions LLC",
-  "contactInfo" : {
-    "email" : "sean.myers@nbtsolutions.com",
-    "twitter" : "@seandmyers",
-    "github" : "http://github.com/seanmyers",
-    "phone" : "+1 716 359-3586",
+  "name": "Sean Myers",
+  "role": "Co-Founder NBT Solutions LLC",
+  "contactInfo": {
+    "email": "sean.myers@nbtsolutions.com",
+    "twitter": "@seandmyers",
+    "github": "http://github.com/seanmyers",
+    "phone": "+1 716 359-3586"
   },
-  "picture" : "images/sean.png",
-  "welcomeMessage" : "Experienced Geospatial Engineer and Developer",
-  "skills" : ["Product Management", "Geographic Information Systems", "Front End Programming"]
+  "picture": "images/sean.png",
+  "welcomeMessage": "Experienced Geospatial Engineer and Developer",
+  "skills": ["Product Management", "Geographic Information Systems", "Front End Programming"]
 }
 
-var work = {};
-  work.employer = "NBT Solutions";
-  work.startdate = "January 2009";
-  work.title = "Product C-Founder";
-  work.years = 5;
-  work.locaation = "Buffalo, NY";
+var education = {
+  "schools": [
+    {
+      "name": "College of Environmental Science and Forestry",
+      "city": "Syracuse, NY",
+      "degree": "B.S.",
+      "majors": ["Environmental Science"],
+      "dates": "1986",
+      "url": "http://www.esf.edu/"
+    },
+    {
+      "name": "University of New Hampshire",
+      "city": "Durham, NH",
+      "degree": "M.S.",
+      "majors": "Environmental Administration and Management",
+      "dates": "1992",
+      "url": "http://www.unh.edu"
+    }
+  ],
+  "onlineCourses": [
+    {
+      "title": "Front-End Web Developer",
+      "school": "Udacity",
+      "dates": "2015",
+      "url": "http://www.udacity.com/nanodegree"
+    }
+  ]
+}
 
-var education = {};
-  education["name"] = "University of New Hampshire";
-  education["years"] = "1988 - 1992";
-  education["city"] = "Durham, NH";
+var work = {
+  "jobs": [
+    {
+      "employer": "NBT Solutions",
+      "title": "Co-Founder",
+      "dates": "January 2009 - Present",
+      "description": "Co-Founder of a geospatial engineering and application
+      development company. Currently NBT Solutions has 10 employees and offices
+      in Buffalo, NY and Portland, ME."
+    },
+    {
+      "employer": "CDM Smith",
+      "title": "Geospatial Engineer",
+      "dates": "April 2002 - January 2009",
+      "description": "Resonsible for the design and implementation of municipal
+      and utility GIS projects"
+    }
+  ]
+}
 
-var formattedName = HTMLheaderName.replace("%data%",bio.name);
-var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-var formattedEmail = HTMLemail.replace("%data%",bio.contactInfo.email);
-var formattedPicture = HTMLbioPic.replace("%data%",bio.picture);
-var formattedMessage = HTMLWelcomeMsg.replace("%data%",bio.welcomeMessage);
-var formattedTwitter = HTMLtwitter.replace("%data%",bio.contactInfo.twitter);
-var formattedGithub = HTMLgithub.replace("%data%",bio.contactInfo.github);
-
-/* Format employment and education information */
-
-var formattedStart = HTMLworkStart.replace("%data%",work.startdate);
-var formattedEmployer = HTMLworkEmployer.replace("%data%",work.employer);
-
-var formattedSchool = HTML
-
-$("#header").prepend(formattedMessage);
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-$("#header").prepend(formattedPicture);
-$("#topContacts").append(formattedEmail);
-$("#topContacts").append(formattedTwitter);
-$("#topContacts").append(formattedGithub);
-$("#header").append(HTMLskillsStart);
-/* $("#header").append(formattedSkills);
-*/
-for (i = 0; i < bio.skills.length; i++) {
-  var formattedSkills = HTMLskills.replace("%data%",bio.skills[i]);
-  $("#skills").append(formattedSkills);
-};
-
-/* Add formattted employment and educaiton information */
-
-$("#workExperience").prepend(formattedStart);
-$("#workExperience").append(formattedEmployer);
+var projects = {
+  "projects": [
+    {
+      "title": "VETRO FiberMap",
+      "dates": "2015",
+      "description": "SaaS application that supports the marketing, planning,
+      and management of broadband networks",
+      "images": [
+        "https://s3.amazonaws.com/Myers/ProjectImages/DemandGen.png",
+        "https://s3.amazonaws.com/Myers/ProjectImages/NetView.png"
+      ]
+    }
+  ]
+}
